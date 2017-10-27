d1475aa63c9fd593820f3ff46b486349b2372274
Guilherme Maia <maialuisguilherme@gmail.com>
2017-10-27 14:05:34 -0300

    feat(search) add search coins section
    add a mock coins list data because of cors issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { IonicStorageModule } from '@ionic/storage/es2015';
-import { HttpClientModule } from '@angular/common/http';
-import { SearchPage } from '../pages/search/search';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpClientModule } from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
 import { DataProvider } from '../providers/data/data';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,8 @@ import { DataProvider } from '../providers/data/data';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SearchPage
   ],
   providers: [
     StatusBar,
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,20 +1,38 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import "rxjs/add/operator/map";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataProvider {
-  result: any;
-  baseUrl = 'https://min-api.cryptocompare.com/data';
 
-  constructor(public _http: HttpClient) {}
+  result:any;
+
+  constructor(public _http: HttpClient) {
+    
+  }
 
   getCoins(coins) {
-    let coinsList = '';
+    let coinlist = '';
 
-    coinsList = coins.join();
+    coinlist = coins.join();
 
-    return this._http.get(`${this.baseUrl}/pricemulti?fsyms=${coinsList}&tsyms=USD`)
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+coinlist+"&tsyms=USD")
+      .map(result => this.result = result);
+  }
+
+  getCoin(coin) {
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+coin+"&tsyms=USD")
+      .map(result => this.result = result);
+  }
+
+  getChart(coin) {
+    return this._http.get("https://min-api.cryptocompare.com/data/histoday?fsym="+coin+"&tsym=USD&limit=30&aggregate=1")
     .map(result => this.result = result);
   }
+
+  allCoins() {
+      return this._http.get("../assets/mock/coins.json")
+        .map(result => this.result = result);
+  }
+
 }
